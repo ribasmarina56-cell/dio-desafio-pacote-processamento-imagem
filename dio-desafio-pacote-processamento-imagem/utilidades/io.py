0dc2dcd7c4a5019855157ebe5978acd7e279874d
ribasmarina56-cell <ribasmarina56@gmail.com>
2026-08-13 20:33:55 -0300

    Atualizar o io.py

--- dio-desafio-pacote-processamento-imagem/utilidades/io.py
+++ dio-desafio-pacote-processamento-imagem/utilidades/io.py
@@ -1,8 +1,26 @@
 from skimage.io import imread, imsave
 
-def ler_imagem(path, is_gray = False):
-    image = imread(path, as_gray = is_gray)
-    return image
 
-def salvar_image(image, path):
+def ler_imagem(path, is_gray=False):
+    """
+    Carrega uma imagem a partir de um arquivo.
+
+    Args:
+        path (str): Caminho do arquivo da imagem.
+        is_gray (bool): Se True, carrega a imagem em escala de cinza.
+
+    Returns:
+        numpy.ndarray: Imagem carregada.
+    """
+    return imread(path, as_gray=is_gray)
+
+
+def salvar_imagem(image, path):
+    """
+    Salva uma imagem em um arquivo.
+
+    Args:
+        image (numpy.ndarray): Imagem que será salva.
+        path (str): Caminho onde a imagem será salva.
+    """
     imsave(path, image)
